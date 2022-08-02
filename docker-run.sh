502057b43ac8aa947cd52ca90546c4ac1a01cd6b
Hike Danakian <hike@hdanak.com>
2022-08-01 23:22:07 -0700

    Add support for running in Docker container

---
+++ docker-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+mkdir -p docker-gradle
+docker run --rm -ti \
+	--user "$(id -u):$(id -g)" \
+    --device /dev/snd \
+    -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
+	-v "$PWD/docker-gradle":/.gradle -e GRADLE_USER_HOME=/.gradle \
+    -v "$PWD":/app -w /app $*
