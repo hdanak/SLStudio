48f91bde11a1b5194fdbdf4f9d30a8ec545d5cee
Hike Danakian <hike@hdanak.com>
2022-08-06 00:25:32 -0700

    Share first graphics card with Docker

--- docker-run.sh
+++ docker-run.sh
@@ -7,6 +7,7 @@ mkdir -p docker-gradle
 docker run --rm -ti \
 	--user "$(id -u):$(id -g)" \
     --device /dev/snd \
+    --device /dev/dri/card0 \
     -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
 	-v "$PWD/docker-gradle":/.gradle -e GRADLE_USER_HOME=/.gradle \
     -v "$PWD":/app -w /app $*
